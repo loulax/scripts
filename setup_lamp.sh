71e746f60d1d4b561a6a4f642d835a04166e27be
loulax <loulaxx91@gmail.com>
2022-08-15 10:26:56 +0000

    remove command in script

--- setup_lamp.sh
+++ setup_lamp.sh
@@ -3,4 +3,3 @@
 apt update && apt -y full-upgrade
 apt -y install apache2 libapache2-mod-php php php-mysql mariadb-server mariadb-client php-{gd,mbstring,json,ctype,xml,simplexml,dom,zip,ldap,curl,sqlite3,mysql} snapd
 echo "ServerSignature Off" >> /etc/apache2/apache2.conf
-snap install code --classic
