6f1ad1b86341d76ab441a92d411ccba3ace8e689
loulax <gestion-loulax@proton.me>
2023-07-16 14:53:22 +0200

    correct file and optimization

--- setup_fw.sh
+++ setup_fw.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
-black='\e[0;30m'
-grey='\e[1;30m'
-darkred='\e[0;31m'
-pink='\e[1;31m'
-darkgreen='\e[0;32m'
-lightgreen='\e[1;32m'
-orange='\e[0;33m'
-yellow='\e[1;33m'
-darkblue='\e[0;34m'
-lightblue='\e[1;34m'
-darkpurple='\e[0;35m'
-lightpurple='\e[1;35m'
-darkcyan='\e[0;36m'
-lightcyan='\e[1;36m'
-lightgrey='\e[0;37m'
-white='\e[1;37m'
-color_off='\e[0;m'
+# black='\e[0;30m'
+# grey='\e[1;30m'
+# darkred='\e[0;31m'
+# pink='\e[1;31m'
+# darkgreen='\e[0;32m'
+# lightgreen='\e[1;32m'
+# orange='\e[0;33m'
+# yellow='\e[1;33m'
+# darkblue='\e[0;34m'
+# lightblue='\e[1;34m'
+# darkpurple='\e[0;35m'
+# lightpurple='\e[1;35m'
+# darkcyan='\e[0;36m'
+# lightcyan='\e[1;36m'
+# lightgrey='\e[0;37m'
+# white='\e[1;37m'
+# color_off='\e[0;m'
 ip_public="dig +short txt ch whoami.cloudflare @1.0.0.1"
 wan_int_name="ip link | awk -F: '$0 !~ 'lo|vir|wl|^[^0-9]'{print $2;getline}'"
+dir_systemd="/lib/systemd/system"
+dir_fw_rules="/etc/init.d"
 
 function banner(){
 
@@ -62,8 +64,9 @@ function check_firewall_service(){
 }
 
 function install_firewall_service(){
+    apt update && apt -y full-upgrade
     apt -y install iptables
-    cat > /lib/systemd/system/firewall.service <<EOF
+    cat > $dir_systemd/firewall.service <<EOF
 [Unit]
 Description=Firewall service
 Requires=network-online.target
@@ -73,14 +76,14 @@ After=network-online.target
 User=root
 Type=oneshot
 RemainAfterExit=yes
-ExecStart=/etc/init.d/fw_on.sh start
-ExecStop=/etc/init.d/fw_off.sh stop
+ExecStart=$dir_fw_rules/fw_on.sh start
+ExecStop=$dir_fw_rules/fw_off.sh stop
 
 [Install]
 WantedBy=multi-user.target
 EOF
-chmod +x /etc/init.d/fw_on.sh
-chmod +x /etc/init.d/fw_off.sh
+chmod +x $dir_fw_rules/fw_on.sh
+chmod +x $dir_fw_rules/fw_off.sh
 systemctl daemon-reload
 read -p "Do you want to start firewall service ? Y/n : " start_firewall
 
@@ -102,7 +105,7 @@ fi
 
 function deployment_rules(){
 
-    cat > /etc/init.d/fw_on.sh <<EOF
+    cat > $dir_fw_rules/fw_on.sh <<EOF
 #!/bin/bash
 
 iptables -F
@@ -116,31 +119,26 @@ iptables -P INPUT DROP
 iptables -P OUTPUT DROP
 iptables -P FORWARD DROP
 
+#APT
 iptables -A INPUT -p tcp -m multiport --sports 80,443 -m conntrack --ctstate ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -p tcp -m multiport --dports 80,443 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
-EOF
-
-read -p "A webserver is running on this machine ? Y/n : " web_server
-
-if [ $web_server = "Y" ] || [ $web_server = "y" ]; then
-
-    read -p "Give the port which is use for : " web_server_port
-
-    cat >> fw_on.sh <<EOF
-
-## TCP/IN => $web_server_port
-iptables -A INPUT -p tcp --dport $web_server_port -m conntrack --ctststate NEW,ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -p tcp --sport $web_server_port -m conntrack --ctstate ESTABLISHED -j ACCEPT
+iptables -A INPUT -p udp --sport 53 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -p udp --dport 53 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 
 EOF
+
+
+if systemctl list-units --full -all | grep -Fq "apache2" || systemctl list-units --full -all | grep -Fq "nginx"; then
+
+    cat >> /etc
+
 fi
-
 
 if systemctl list-units --full -all | grep -Fq "ssh"; then
 
     read -p "Enter the listening ssh port : " ssh_server_port
 
-    cat >> fw_on.sh <<EOF
+    cat >> $dir_fw_rules/fw_on.sh <<EOF
 
 ## TCP/IN => $ssh_server_port
 iptables -A INPUT -p tcp --dport $ssh_server_port -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
@@ -169,7 +167,7 @@ EOF
 fi
 
 
-cat > /etc/init.d/fw_off.sh <<EOF
+cat > $dir_fw_rules/fw_off.sh <<EOF
 #!/bin/bash
 
 iptables -F
