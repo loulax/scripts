1a5c7171072c5510ecf14c744e99f7780d0c087b
Loulax <gestion-loulax@proton.me>
2024-07-03 22:53:26 +0200

    replaced config in /etc/apache2/sites-enabled and enable ldap

--- setup_glpi.sh
+++ setup_glpi.sh
@@ -1,20 +1,3 @@
-#########################################
-                                        #
-    SCRIPT INSTALLATION GLPI            #
-                                        #
-    Author : Louis Arnau                #
-    Version : 1.1                       #
-                                        #
-#########################################
-
-
- Mise à jour du système
- Installation des dépendances
- GLPI est déjà installé.
- Redémarrage du service apache2 / mariadb
- L'installation est log dans /var/log/glpi_setup.log
-root@glpi:~# nano setup_glpi.sh
-root@glpi:~# cat setup_glpi.sh
 #!/bin/bash
 
 black='\e[0;30m'
@@ -42,7 +25,6 @@ echo -e """ $darkpurple
     SCRIPT INSTALLATION GLPI            #
                                         #
     Author : Louis Arnau                #
-    Enterprise : Cyberzen               #
     Version : 1.0                       #
                                         #
 #########################################
@@ -62,7 +44,7 @@ apt update &>/dev/null 2>&1 >>/var/log/glpi_setup.log && apt -y full-upgrade &>/
 echo -e "$lightblue Installation des dépendances $color_off"
 #Installation d'apache et des paquets php nécessaires au bon fonctionnement de glpi
 apt -y install apache2 libapache2-mod-php php php-mysql mariadb-server &>/dev/null 2>&1 >>/var/log/glpi_setup.log
-apt -y install php-{ldap,cli,fileinfo,json,xmlrpc,imap,apcu,apcu-bc,xmlrpc,cas,mysqli,mbstring,curl,gd,simplexml,xml,intl,zip,bz2,fpm} &>/dev/null 2>&1 >>/var/log/glpi_setup.log
+apt -y install php-{ldap,cli,curl,xmlreader,xmlwriter,intl,bz2,zip,fileinfo,json,xmlrpc,imap,apcu,apcu-bc,xmlrpc,cas,mysqli,mbstring,curl,gd,simplexml,xml,intl,zip,bz2,fpm} &>/dev/null 2>&1 >>/var/log/glpi_setup.log
 
 if [[ -d /var/www/html/glpi ]]; then
 
@@ -71,7 +53,6 @@ if [[ -d /var/www/html/glpi ]]; then
 else
 
     echo -e "$lightblue Récupération de l'archive depuis internet $color_off"
-    tail /var/log/glpi_setup.log
     cd /tmp && wget https://github.com/glpi-project/glpi/releases/download/10.0.16/glpi-10.0.16.tgz &>/dev/null 2>&1 >>/var/log/glpi_setup.log
     echo -e "$lightblue extraction de l'archive $color_off"
     cd /tmp && tar xvzf glpi-*.tgz -C /var/www/html &>/dev/null 2>&1 >>/var/log/glpi_setup.log
@@ -97,6 +78,37 @@ else
         Require all granted
         </Directory>
     ServerSignature Off" >> /etc/apache2/apache2.conf
+    echo "<VirtualHost *:80>
+	# The ServerName directive sets the request scheme, hostname and port that
+	# the server uses to identify itself. This is used when creating
+	# redirection URLs. In the context of virtual hosts, the ServerName
+	# specifies what hostname must appear in the request's Host: header to
+	# match this virtual host. For the default virtual host (this file) this
+	# value is not decisive as it is used as a last resort host regardless.
+	# However, you must set it for any further virtual host explicitly.
+	#ServerName www.example.com
+
+	ServerAdmin webmaster@localhost
+	DocumentRoot /var/www/html/glpi/
+
+	# Available loglevels: trace8, ..., trace1, debug, info, notice, warn,
+	# error, crit, alert, emerg.
+	# It is also possible to configure the loglevel for particular
+	# modules, e.g.
+	#LogLevel info ssl:warn
+
+	ErrorLog ${APACHE_LOG_DIR}/error.log
+	CustomLog ${APACHE_LOG_DIR}/access.log combined
+
+	# For most configuration files from conf-available/, which are
+	# enabled or disabled at a global level, it is possible to
+	# include a line for only one particular virtual host. For example the
+	# following line enables the CGI configuration for this host only
+	# after it has been globally disabled with "a2disconf".
+	#Include conf-available/serve-cgi-bin.conf
+</VirtualHost>" > /etc/apache2/sites-enabled/glpi.conf
+	/usr/sbin/a2dissite 000-default.conf
+    a2enmod ldap
 fi
 
 echo -e "$darkgreen Redémarrage du service apache2 / mariadb $color_off"
